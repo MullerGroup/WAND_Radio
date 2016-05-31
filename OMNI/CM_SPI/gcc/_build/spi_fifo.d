92bf6a20926221c155938d59bcf6c365a3b6f234
BWRC <bwrc@dhcp-63-148.EECS.Berkeley.EDU>
2016-05-31 11:42:57 -0700

    - added some debug info to the CM side

--- OMNI/CM_SPI/gcc/_build/spi_fifo.d
+++ OMNI/CM_SPI/gcc/_build/spi_fifo.d
@@ -1,7 +1,7 @@
 _build/spi_fifo.o: ../spi_fifo.c ../spi_fifo.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/stdint.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/machine/_default_types.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/features.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/_intsup.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/_stdint.h
+ /Users/bwrc/OMNI/radio/gcc-arm-none-eabi-4_9-2015q2/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /Users/bwrc/OMNI/radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/stdint.h \
+ /Users/bwrc/OMNI/radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/machine/_default_types.h \
+ /Users/bwrc/OMNI/radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/features.h \
+ /Users/bwrc/OMNI/radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/_intsup.h \
+ /Users/bwrc/OMNI/radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/_stdint.h
