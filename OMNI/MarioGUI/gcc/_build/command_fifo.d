5c85dc652fa6d2baea72df90fce0f2edfdeae6a6
George Alexandrov <gpalex@berkeley.edu>
2016-11-16 13:28:10 -0800

    added .gitignore file to ignore Makefiles

--- OMNI/MarioGUI/gcc/_build/command_fifo.d
+++
@@ -1,7 +0,0 @@
-_build/command_fifo.o: ../command_fifo.c ../command_fifo.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/stdint.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/machine/_default_types.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/features.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/_intsup.h \
- /Users/Andy/Desktop/Mario_Radio/gcc-arm-none-eabi-4_9-2015q2/arm-none-eabi/include/sys/_stdint.h
